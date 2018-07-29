e0a4765f9092e258361d3637d9b1953d2627ff31
Gronlund <502614626@ge.com>
2018-07-29 11:18:03 +0200

    Simple test project that uses rest api from netCore and netFramework

---
+++ NetCoreConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        try
+	        {
+				Console.WriteLine(Assembly.GetCallingAssembly());
+		        HttpWebRequest request = (HttpWebRequest) HttpWebRequest.Create(@"https://reqres.in/api/users?page=2");
+		        request.Method = "GET";
+		        String test = String.Empty;
+		        using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+		        {
+			        Stream dataStream = response.GetResponseStream();
+			        StreamReader reader = new StreamReader(dataStream);
+			        test = reader.ReadToEnd();
+			        Console.WriteLine(test);
+			        reader.Close();
+			        dataStream.Close();
+		        }
+	        }
+	        catch (Exception ex)
+	        {
+				Console.WriteLine("Failed with exception: " + ex.Message + Environment.NewLine + ex.StackTrace);
+	        }
+
+	        Console.ReadKey();
+        }
+    }
+}
---
+++ NetFrameworkConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrameworkConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try {
+				Console.WriteLine(Assembly.GetCallingAssembly());
+				HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(@"https://reqres.in/api/users?page=2");
+				request.Method = "GET";
+				String test = String.Empty;
+				using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+				{
+					Stream dataStream = response.GetResponseStream();
+					StreamReader reader = new StreamReader(dataStream);
+					test = reader.ReadToEnd();
+					Console.WriteLine(test);
+					reader.Close();
+					dataStream.Close();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Failed with exception: " + ex.Message + Environment.NewLine + ex.StackTrace);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
